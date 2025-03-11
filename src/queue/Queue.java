a111a01bac62270f96e45e25923a9d8fc5ef16fc
Khaik5 <vietkhai14012005@gmail.com>
2025-03-11 22:00:10 +0700

    Da xong

---
+++ src/queue/Queue.java
@@ -0,0 +1,113 @@
+package queue;
+
+import java.util.Scanner;
+
+class Node {
+
+    int data;
+    Node next;
+
+    public Node(int x) {
+        data = x;
+        next = null;
+    }
+
+    @Override
+    public String toString() {
+        return data + "->";
+
+    }
+}
+
+public class Queue {
+
+    Node head;
+    Node tail;
+
+    void MyQueue() {
+        head = tail = null;
+    }
+
+    boolean EmptQ() {
+        return head == null;
+    }
+
+    void add(int x) {
+        Node t = new Node(x);
+        if (head == null) {
+            head = tail = t;
+
+        } else {
+            tail.next = t;
+            tail = t;
+        }
+    }
+
+    int RemoveQ() {
+        int x = 0;
+        if (head == null) {
+
+            System.out.println("\n Hang doi rong");
+
+        } else {
+            x = head.data;
+            if (head.next == null) {
+                head = tail = null;
+            } else {
+                head = head.next;
+            }
+        }
+        return x;
+    }
+
+    void nhap() {
+        Scanner sc = new Scanner(System.in);
+        int x;
+        while (true) {
+            System.out.println("Nhap x<>0:");
+            x = sc.nextInt();
+            if (x == 0) {
+                break;
+            } else {
+                add(x);
+            }
+        }
+    }
+
+    void tong() {
+        Queue q = new Queue();
+        int k = 0;
+        while (!EmptQ()) {
+            int x = RemoveQ();
+            k = k + x;
+            q.add(x);
+
+        }
+        while (!q.EmptQ()) {
+            this.add(q.RemoveQ());
+        }
+        
+    }
+
+    void in() {
+        System.out.println("\n Noi dung hang doi: ");
+        Queue q = new Queue();
+        while (!EmptQ()) {
+            int x = RemoveQ();
+            System.out.println(" " + x);
+            q.add(x);
+        }
+        while (!q.EmptQ()) {
+            this.add(q.RemoveQ());
+        }
+    }
+
+    public static void main(String[] args) {
+        Queue q = new Queue();
+
+        q.nhap();
+        q.in();
+        q.tong();
+
+    }
+}
